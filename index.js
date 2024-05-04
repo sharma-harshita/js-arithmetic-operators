0280e6f92cc004e702d2f55720464efaf74befc2
Harshita Sharma <sharmaharshita711@gmail.com>
2024-05-04 09:19:11 +0530

    changes

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("hello")
+// // addition + - * / %
+
+// // ** = exponential operator
+// console.log(5 ** 2);
+
+let a = 5;
+let b = a++;  //post increment
+// let b = ++a;  //post increment
+console.log("a , b", a , b);
+
+
+let x = 10;
+// let y = x--;
+let y = --x;
+console.log(x, y);
